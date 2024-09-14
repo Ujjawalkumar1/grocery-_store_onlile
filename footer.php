80c84451759f6200e84e9d189fd4d3bcb6c47619
Ujjawal Kumar <ujjawalkumar787898@gmail.com>
2024-09-15 03:48:14 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 9647941037 </p>
+         <p> <i class="fas fa-phone"></i> 7562072047 </p>
+         <p> <i class="fas fa-envelope"></i> skjaigaon5@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Jaigaon-736182 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100008827416445" target="_blank"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/ujjawal_kuma_r/" target="_blank"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/ujjawalk1/" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+
+
+
+
+
+   </section>
+
+   <p class="credit"> &copy; copyright @ <?= date('Y'); ?> by <span>mr. Santosh Kumar</span> | all rights reserved! </p>
+
+</footer>
